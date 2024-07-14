9cf8ea0d33c1d97bafb1514c75dc6bc642ed9813
Piyush <b521044@iiit-bh.ac.in>
2024-07-15 04:23:23 +0530

    Add get airplane api

--- src/controllers/airplane-controller.js
+++ src/controllers/airplane-controller.js
@@ -21,6 +21,11 @@ async function createAirplane(req, res) {
   }
 }
 
+/**
+ * POST : /airplanes/
+ * req-body {}
+ */
+
 async function getAirplanes(req, res) {
   try {
     const airplanes = await AirplaneService.getAirplanes();
@@ -32,7 +37,27 @@ async function getAirplanes(req, res) {
   }
 }
 
+
+/**
+ * POST : /airplanes/:id 
+ * req-body {}
+ */
+
+async function getAirplane(req, res) {
+  try {
+    const airplanes = await AirplaneService.getAirplane(req.params.id);
+    SuccessResponse.data = airplanes;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
+
+
 module.exports = {
   createAirplane,
-  getAirplanes
+  getAirplanes,
+  getAirplane,
 };
--- src/services/airplane-sevice.js
+++ src/services/airplane-sevice.js
@@ -1,36 +1,60 @@
-const {AirplaneRepository} = require('../repositories');
-const {StatusCodes} = require('http-status-codes');
-const AppError = require('../utils/errors/app-error');
-
+const { AirplaneRepository } = require("../repositories");
+const { StatusCodes } = require("http-status-codes");
+const AppError = require("../utils/errors/app-error");
 
 const airPlaneRepository = new AirplaneRepository();
 
-async function createAirplane(data){
-    try {
-        const airplane = await airPlaneRepository.create(data);
-        return airplane;
-    } catch (error) {
-        if(error.name == 'SequelizeValidationError') {
-            let explanation = [];
-            error.errors.forEach((err) => {
-                explanation.push(err.message);
-            });
-            throw new AppError(explanation, StatusCodes.BAD_REQUEST);
-        }
-        throw new AppError('Cannot create a new Airplance object', StatusCodes.INTERNAL_SERVER_ERROR);
+async function createAirplane(data) {
+  try {
+    const airplane = await airPlaneRepository.create(data);
+    return airplane;
+  } catch (error) {
+    if (error.name == "SequelizeValidationError") {
+      let explanation = [];
+      error.errors.forEach((err) => {
+        explanation.push(err.message);
+      });
+      throw new AppError(explanation, StatusCodes.BAD_REQUEST);
     }
+    throw new AppError(
+      "Cannot create a new Airplance object",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
 }
 
-async function getAirplanes(){
-    try {
-        const airplanes = await airPlaneRepository.getAll();
-        return airplanes;
-    } catch (error) {
-        throw new AppError('Cannot fetch data of all the airplanes', StatusCodes.INTERNAL_SERVER_ERROR);
+async function getAirplanes() {
+  try {
+    const airplanes = await airPlaneRepository.getAll();
+    return airplanes;
+  } catch (error) {
+    throw new AppError(
+      "Cannot fetch data of all the airplanes",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+async function getAirplane(id) {
+  try {
+    const airplane = await airPlaneRepository.get(id);
+    return airplane;
+  } catch (error) {
+    if (error.statusCode == StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        "The airplane you requested is not present",
+        error.statusCode
+      );
     }
+    throw new AppError(
+      "Cannot fetch data of all the airplanes",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
 }
 
 module.exports = {
-    createAirplane,
-    getAirplanes
-}
+  createAirplane,
+  getAirplanes,
+  getAirplane,
+};
