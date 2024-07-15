c46ac7bf33e3cab2e233c5ff6cd364b4a5701284
Piyush <b521044@iiit-bh.ac.in>
2024-07-15 13:10:26 +0530

    add create city api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {InfoController} = require('../../controllers');
+const {InfoController, CityController} = require('../../controllers');
 const airPlaneRoutes = require('./airplane-routes');
+const cityRoutes = require('./city-routes');
 
 router.get('/info',InfoController.info);
 router.use('/airplanes',airPlaneRoutes);
+router.use('/cities',cityRoutes);
+
 
 module.exports = router;
--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,4 @@
 module.exports = {
     AirplaneService: require('./airplane-sevice'),
+    CityService: require('./city-service')
 }
