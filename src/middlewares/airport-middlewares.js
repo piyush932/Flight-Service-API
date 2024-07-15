4c45334e3fb8905d4ad74a589c60485cf45e16e8
Piyush <b521044@iiit-bh.ac.in>
2024-07-16 01:12:51 +0530

    add airport resource crud

---
+++ src/middlewares/airport-middlewares.js
@@ -0,0 +1,59 @@
+const { StatusCodes } = require('http-status-codes');
+
+const { ErrorResponse } = require('../utils/comman');
+const AppError = require('../utils/errors/app-error');
+
+function validateCreateRequest(req, res, next) {
+    if(!req.body.name) {
+        ErrorResponse.message = 'Something went wrong while creating airport';
+        ErrorResponse.error = new AppError(['name not found in the oncoming request in the correct form'], StatusCodes.BAD_REQUEST);
+        return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json(ErrorResponse);
+    }
+    if(!req.body.code) {
+        ErrorResponse.message = 'Something went wrong while creating airport';
+        ErrorResponse.error = new AppError(['Airport code not found in the oncoming request in the correct form'], StatusCodes.BAD_REQUEST);
+        return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json(ErrorResponse);
+    }
+    if(!req.body.cityId) {
+        ErrorResponse.message = 'Something went wrong while creating airport';
+        ErrorResponse.error = new AppError(['City Id not found in the oncoming request in the correct form'], StatusCodes.BAD_REQUEST);
+        return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json(ErrorResponse);
+    }
+    next();
+}
+
+const validateUpdateRequest = (req, res, next) => {
+    if(!req.body.name) {
+        ErrorResponse.message = 'Something went wrong while updating airport';
+        ErrorResponse.error = new AppError(['name not found in the oncoming request in the correct form'], StatusCodes.BAD_REQUEST);
+        return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json(ErrorResponse);
+    }
+    if(!req.body.code) {
+        ErrorResponse.message = 'Something went wrong while updating airport';
+        ErrorResponse.error = new AppError(['Airport code not found in the oncoming request in the correct form'], StatusCodes.BAD_REQUEST);
+        return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json(ErrorResponse);
+    }
+    if(!req.body.cityId) {
+        ErrorResponse.message = 'Something went wrong while updating airport';
+        ErrorResponse.error = new AppError(['City Id not found in the oncoming request in the correct form'], StatusCodes.BAD_REQUEST);
+        return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json(ErrorResponse);
+    }
+    next();
+}
+
+module.exports = {
+    validateCreateRequest,
+    validateUpdateRequest
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const {InfoController, CityController} = require('../../controllers');
 const airPlaneRoutes = require('./airplane-routes');
 const cityRoutes = require('./city-routes');
+const airportRoutes = require('./airport-routes');
 
 router.get('/info',InfoController.info);
 router.use('/airplanes',airPlaneRoutes);
 router.use('/cities',cityRoutes);
+router.use('/airports', airportRoutes);
 
 
 module.exports = router;
