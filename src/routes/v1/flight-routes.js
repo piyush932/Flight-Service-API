85f8112d06441c722e88a20e383524cde3d58f00
Piyush <b521044@iiit-bh.ac.in>
2024-07-17 14:39:36 +0530

    add flight seats update api

--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -1,21 +1,28 @@
-const express = require('express');
+const express = require("express");
 
-const { FlightController } = require('../../controllers');
-const { FlightMiddlewares } = require('../../middlewares');
+const { FlightController } = require("../../controllers");
+const { FlightMiddlewares } = require("../../middlewares");
 
 const router = express.Router();
 
 // /api/v1/flights POST
-router.post('/', 
-        FlightMiddlewares.validateCreateRequest,
-        FlightController.createFlight);
+router.post(
+  "/",
+  FlightMiddlewares.validateCreateRequest,
+  FlightController.createFlight
+);
 
 // /api/v1/flights?trips=MUM-DEL GET
-router.get('/', 
-        FlightController.getAllFlights);
+router.get("/", FlightController.getAllFlights);
 
 // /api/v1/flights/:id GET
-router.get('/:id', 
-        FlightController.getFlight);
+router.get("/:id", FlightController.getFlight);
+
+// /api/v1/flights/:id/seats PATCH
+router.patch(
+  "/:id/seats",
+  FlightMiddlewares.validateUpdateSeatsRequest,
+  FlightController.updateSeats
+);
 
 module.exports = router;
